ea6de05dfdabe3e53cc9a0fcd8e575ef2b349de6
nicolelinh444 <nicolelinh444@gmail.com>
2025-10-24 11:32:24 -0700

    added pizza party planner

--- pizzaPartyPlanner/pizzaPartyPlanner.js
+++ pizzaPartyPlanner/pizzaPartyPlanner.js
@@ -5,3 +5,17 @@ Your task: Make a Node.js pizza party planner that calculates how many slices of
 Follow the instructions in the README.md file in this folder.
 
 */
+
+let hostName = process.argv[2];
+let pizzasOrdered = Number(process.argv[3]);
+let slicesPerPizza = Number(process.argv[4]);
+let numberOfGuests = Number(process.argv[5]);
+
+let totalSlices = slicesPerPizza * pizzasOrdered;
+
+let slicesPerGuest = Math.floor(totalSlices / numberOfGuests);
+let leftOverSlices = totalSlices % numberOfGuests;
+
+console.log(`Hey ${hostName}! 🍕 You ordered ${pizzasOrdered} pizzas with ${slicesPerPizza} each - that's ${totalSlices} slices total!
+    Each of your ${numberOfGuests} guests gets ${slicesPerGuest} slices, and you'll have ${leftOverSlices} left over.
+    Save those for breakfast! 😋`);5
